9d7447a5ffb97fa8744dc828f64dd0dfca051402
edward wang <edwardwang@edwards-MBP.home>
2019-11-03 20:23:36 -0500

    created project AWS_StudentManagement

---
+++ src/main/java/com/mycompany/myapp/service/CourseService.java
@@ -0,0 +1,65 @@
+package com.mycompany.myapp.service;
+
+import com.mycompany.myapp.domain.Course;
+import com.mycompany.myapp.domain.User;
+import com.mycompany.myapp.domain.UserCourse;
+import com.mycompany.myapp.domain.dto.CourseDto;
+import com.mycompany.myapp.domain.dto.CourseWithTNDto;
+import com.mycompany.myapp.repository.CourseRepository;
+import com.mycompany.myapp.repository.UserCourseRepository;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CourseService {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private UserCourseRepository userCourseRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public List<CourseDto> findAllCourses(){
+        List<Course> courses= courseRepository.findAll();
+
+        List<CourseDto> courseDtos = new ArrayList<>();
+
+        for(Course c : courses) {
+            courseDtos.add(new CourseDto(c.getCourseName(), c.getCourseLocation(), c.getCourseContent(), c.getTeacherId()));
+        }
+
+        return courseDtos;
+    }
+
+    public List<CourseDto> findAllCoursesDtoFromDB(){
+        return courseRepository.findAllCoursesDto();
+    }
+
+    public List<CourseWithTNDto> findAllCoursesDtoWithTeacherNameFromDB(){
+        return courseRepository.findAllCoursesDtoWithTeacherName();
+    }
+
+
+    public void registerCourse(String courseName) throws Exception{
+        Optional<User> curUser = userService.getUserWithAuthorities();
+        Optional<Course> curCourse = courseRepository.findCourseByCourseName(courseName);
+
+        if (curUser.isPresent() && curCourse.isPresent()){
+            userCourseRepository.save(UserCourse.builder()
+                .user(curUser.get())
+                .course(curCourse.get())
+                .build());
+        } else {
+            throw new Exception("UnExpected Exception");
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/myapp/web/rest/CourseController.java
@@ -0,0 +1,52 @@
+package com.mycompany.myapp.web.rest;
+
+import com.mycompany.myapp.domain.dto.CourseDto;
+import com.mycompany.myapp.domain.dto.CourseWithTNDto;
+import com.mycompany.myapp.service.CourseService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@Api(value="Course Service Controller", description = "Controller for find couses information")
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping(path = "/api/course/findAllCourses", produces = "application/json")
+    public HttpEntity<List<CourseDto>> findAllCourses(){
+        List<CourseDto> allCourses = courseService.findAllCourses();
+
+        return new ResponseEntity<>(allCourses, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/api/course/findAllCoursesDto", produces = "application/json")
+    public HttpEntity<List<CourseDto>> findAllCoursesDto(){
+        List<CourseDto> allCourses = courseService.findAllCoursesDtoFromDB();
+
+        return new ResponseEntity<>(allCourses, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/api/course/findAllCoursesWithTNDto", produces = "application/json")
+    public HttpEntity<List<CourseWithTNDto>> findAllCoursesWithTNDto(){
+        List<CourseWithTNDto> allCourses = courseService.findAllCoursesDtoWithTeacherNameFromDB();
+
+        return new ResponseEntity<>(allCourses, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/api/course/registerCourse/{courseName}", produces = "application/json")
+    public HttpStatus registerCourse(@PathVariable String courseName) {
+        try {
+            courseService.registerCourse(courseName);
+            return HttpStatus.OK;
+        } catch (Exception e) {
+            return HttpStatus.UNPROCESSABLE_ENTITY;
+        }
+    }
+}
